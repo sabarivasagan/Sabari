feeb5294537c2e14a7c01125953900eaaa2e94b1
Sabari vasagan <vasagansabari@gmail.com>
2020-05-07 14:17:44 +0530

    React Bootstrap
    Imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./styles.css";
+import { Button } from 'react-bootstrap';
+
+class Header extends React.Component
+{
+  render()
+  {
+    return (
+      <div>
+        <Button variant="primary">Primary</Button>
+    </div>
+    );
+  }
+}
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
