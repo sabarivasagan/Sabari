ec3a1d935c1a0643909083329adb169006eeb258
Sabari vasagan <vasagansabari@gmail.com>
2020-05-07 15:06:02 +0530

    Header Added
    Changes made for Header file

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,22 +1,12 @@
 import React from "react";
+import { Navbar } from "react-bootstrap";
 
-class Header extends React.Component 
-{
-  render() 
-  {
+export class Header extends React.Component {
+  render() {
     return (
       <div>
         <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            React Bootstrap
-          </Navbar.Brand>
+          <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
         </Navbar>
       </div>
     );
